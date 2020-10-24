8d16414a8b086a0302f9633fdd3879c443594f67
jeanpaul <jepebe@users.noreply.github.com>
2020-10-24 16:16:13 +0200

    Bump version to 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pxng",
-    version="0.0.7",
+    version="0.0.8",
     author="Jean-Paul Balabanian",
     author_email="jepebe@prador.net",
     description="A library for fiddling with pixels",
